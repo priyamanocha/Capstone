8257dec6d5a70a5072aff82b8ee4d7f4155ec581
priyamanochamalhi <146389300+priyamanochamalhi@users.noreply.github.com>
2024-06-03 15:01:57 -0400

    Added insert queries for categories, sub-categories, services and mapping

--- database/residence_revive.sql
+++ database/residence_revive.sql
@@ -88,5 +88,70 @@ CREATE TABLE IF NOT EXISTS booking_services (
     PRIMARY KEY(booking_id, category_id, service_id)
 );
 
+-- Insert Queries for Categories
+insert into categories values(1, 'Cleaning/Disinfection');
+insert into categories values(2, 'Plumbing');
+insert into categories values(3, 'Electrician');
+insert into categories values(4, 'Appliance Repair');
+insert into categories values(5, 'Pest-Control');
+insert into categories values(6, 'Furniture Assembly');
 
+-- Insert Queries for Sub-Categories
+-- SET SQL_SAFE_UPDATES = 0;
+insert into sub_categories values(1, 'Bathroom Cleaning');
+insert into sub_categories values(2, 'Kitchen Cleaning');
+insert into sub_categories values(3, 'Carpet Cleaning');
+insert into sub_categories values(4, 'Home Cleaning');
+insert into sub_categories values(5, 'Tap');
+insert into sub_categories values(6, 'Shower');
+insert into sub_categories values(7, 'Basin & Sink');
+insert into sub_categories values(8, 'Bath fittings');
+insert into sub_categories values(9, 'Minor Fitting/Installation');
+insert into sub_categories values(10, 'Toilet');
+insert into sub_categories values(11, 'Others');
+insert into sub_categories values(12, 'Switch and Socket');
+insert into sub_categories values(13, 'Light');
+insert into sub_categories values(14, 'Doorbell');
+insert into sub_categories values(15, 'Television');
+insert into sub_categories values(16, 'Expert Consultation');
+insert into sub_categories values(17, 'Refrigerator');
+insert into sub_categories values(18, 'Microwave');
+insert into sub_categories values(19, 'Induction');
+insert into sub_categories values(20, 'Chimney');
+insert into sub_categories values(21, 'Washer');
+insert into sub_categories values(22, 'Dryer');
+insert into sub_categories values(23, 'Dish Washer');
+insert into sub_categories values(24, 'Wardrobe');
+insert into sub_categories values(25, 'Tables/Drawers/Workspaces');
+insert into sub_categories values(26, 'Bed');
+insert into sub_categories values(27, 'Sofa');
 
+-- Insert Queries for Services
+insert into services values(1, 'Half-Washroom Classic Cleaning', 'Half-Washroom Classic Cleaning', 9.99);
+insert into services values(2, 'Half-Washroom Deep Cleaning', 'Half-Washroom Deep Cleaning', 14.99);
+insert into services values(3, 'Full-Washroom Classic Cleaning', 'Full-Washroom Classic Cleaning', 12.99);
+insert into services values(4, 'Full-Washroom Deep Cleaning', 'Full-Washroom Deep Cleaning', 16.99);
+insert into services values(5, 'Move-In Cleaning', 'Move-in Kitchen Cleaning', 49.99);
+insert into services values(6, 'Shelve Cleaning', 'Kitchen Shelve Cleaning', 20.99);
+insert into services values(7, 'Chimney Cleaning', 'Kitchen Chimney Cleaning', 20.99);
+insert into services values(8, 'Refrigerator Cleaning', 'Refrigerator Cleaning', 20.99);
+insert into services values(9, 'Induction Cleaning', 'Induction Cleaning', 20.99);
+insert into services values(10, 'Microwave Cleaning', 'Induction Cleaning', 20.99);
+insert into services values(11, 'Classic Home Cleaning', 'Classic Home cleaning without Kitchen and Washroom', 99.99);
+insert into services values(12, 'Deep Home Cleaning', 'Deep Home cleaning ', 149.99);
+insert into services values(13, 'Deep Carpet Cleaning', 'Deep Carpet Cleaning (vaccum)', 99.99);
+
+-- Insert Queries for category_subcategory_service_mapping
+insert into category_subcategory_service_mapping values(1, 1, 1);
+insert into category_subcategory_service_mapping values(1, 1, 2);
+insert into category_subcategory_service_mapping values(1, 1, 3);
+insert into category_subcategory_service_mapping values(1, 1, 4);
+insert into category_subcategory_service_mapping values(1, 2, 5);
+insert into category_subcategory_service_mapping values(1, 2, 6);
+insert into category_subcategory_service_mapping values(1, 2, 7);
+insert into category_subcategory_service_mapping values(1, 2, 8);
+insert into category_subcategory_service_mapping values(1, 2, 9);
+insert into category_subcategory_service_mapping values(1, 2, 10);
+insert into category_subcategory_service_mapping values(1, 3, 13);
+insert into category_subcategory_service_mapping values(1, 4, 11);
+insert into category_subcategory_service_mapping values(1, 4, 12);
