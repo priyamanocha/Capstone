327c921f03c4501754980c7515e464c23975a2ed
Jai Maurya <jaimaurya123456@gmail.com>
2024-06-05 15:43:13 -0400

    Update the necessary files for the SignUp

--- signup.php
+++ signup.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up to Residence Revive</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/signup_styles.css">
 </head>
 <body>
     <div class="container mt-5">
