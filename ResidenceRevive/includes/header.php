e5cda65c3162d68ce8d6978fc191ac159a5ccd26
priyamanochamalhi <146389300+priyamanochamalhi@users.noreply.github.com>
2024-06-05 17:08:06 -0400

    Added Header

--- ResidenceRevive/includes/header.php
+++ ResidenceRevive/includes/header.php
@@ -1,21 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
     <header>
-        <h1>Residence Revive</h1>
         <nav>
-            <a href="index.php">Home</a>
-            <a href="services.php">About Us</a>
-            <a href="about.php">Contact Us</a>
-            <a href="contact.php">Cart</a>
-            <a href="signup.php">SignUp</a>
-            <a href="login.php">Login</a>
+            <div class="logo">
+                <a href="#"><img src="images/ResidenceRevive_logo.png" alt="Residence Revive Logo"></a>
+                <a href="#">Residence Revive</a>
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Search for Service" class="search-service">
+            </div>
+            <div class="links">
+                <a href="#">Home</a>
+                <a href="#">About us</a>
+                <a href="#">Contact us</a>
+                <a href="#">Login</a>
+            </div>
+            <div class="menu-toggle">
+                <i class="fas fa-bars"></i>
+            </div>
         </nav>
+
     </header>
     <main>
+    </main>
+</body>
+
+</html>
