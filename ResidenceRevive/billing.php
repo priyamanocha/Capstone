d867954df3f26bc71f8b02112e2211917d39aec5
“vhari4654” <harikrushna194@gmail.com>
2024-07-03 18:01:02 -0400

    Apply Validation on Billing Page Fields.

--- ResidenceRevive/billing.php
+++ ResidenceRevive/billing.php
@@ -1,3 +1,122 @@
+<?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$email = "";
+$fname_err = $lname_err = $cfname_err = $clname_err = $email_err = $card_err = $security_err = $month_err = $year_err = $unit_err = $address_err = $city_err = $state_err = $zip_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+
+    // Personal Information Input Validation.
+
+    if(empty($_POST["fname"]))
+    {
+        $fname_err = "Please Enter First Name!";
+    }
+
+    if(empty($_POST["lname"]))
+    {
+        $lname_err = "Please Enter Last Name!";
+    }
+    
+    if(empty($_POST["email"]))
+    {
+        $email_err = "Please Enter Email Id!";
+    }
+    elseif(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        $email_err = "Invalid email format!";
+    } else {
+        $email = htmlspecialchars($_POST["email"]);
+    }
+
+    // Credit Card Input Validation.
+
+    if(empty($_POST["cfname"]))
+    {
+        $cfname_err = "Please Enter First Name!";
+    }
+    if(empty($_POST["clname"]))
+    {
+        $clname_err = "Please Enter Last Name!";
+    }
+    
+    if(empty($_POST["ccard"]))
+    {
+        $card_err = "Please Enter Card Number!";
+    }
+    elseif(!empty($_POST["ccard"]) && !preg_match("/^[0-9]{12}$/", $_POST["ccard"]))
+    {
+        $card_err = "Please Enter Valid Card Number!";
+    }
+
+    if(empty($_POST["scode"]))
+    {
+        $security_err = "Please Enter Security Code!";
+    }
+    elseif(!empty($_POST["scode"] && !preg_match("/^[0-9]{3} $/", $_POST["scode"])))
+    {
+        $security_err = "Please Enter Valid Security Code!";
+
+    }
+
+    if(empty($_POST["emonth"]))
+    {
+        $month_err = "Please Enter Expiration Month!";
+    }
+    elseif(!empty($_POST["emonth"] && !preg_match("/^[0-9]{2} $/", $_POST["emonth"])))
+    {
+        $month_err = "Please Enter Valid Expiration Month!";
+
+    }
+
+    if(empty($_POST["eyear"]))
+    {
+        $year_err = "Please Enter Expiration Year!";
+    }
+    elseif(!empty($_POST["eyear"] && !preg_match("/^[0-9]{4} $/", $_POST["eyear"])))
+    {
+        $year_err = "Please Enter Valid Expiration Year!";
+    }
+
+    // Unit Information Input Validation.
+
+    if(empty($_POST["unumber"]))
+    {
+        $unit_err = "Please Enter Unit Number!";
+    }
+    elseif(!empty($_POST["unumber"] && !preg_match("/^[0-9]{5} $/", $_POST["unumber"])))
+    {
+        $unit_err = "Please Enter Valid Unit Number!";
+    }
+
+    if(empty($_POST["saddress"]))
+    {
+        $address_err = "Please Enter Street Address!";
+    }
+    if(empty($_POST["city"]))
+    {
+        $city_err = "Please Enter City Name!";
+    }
+    if(empty($_POST["state"]))
+    {
+        $state_err = "Please Enter State/Province Name!";
+    }
+    if(empty($_POST["zcode"]))
+    {
+        $zip_err = "Please Enter Zip Code!";
+    }
+
+}
+
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +132,7 @@
     <?php include 'includes/header.php' ?>
     <main>
         <div class="billing-form-container">
-            <form action="billing_action.php" method="POST" class="fo">
+            <form action="billing.php" method="POST" class="fo">
                 <div>
                     <h1> Personal Information </h1>
                 </div>
@@ -21,15 +140,18 @@
                 <div>
                     <div class="form-group">
                         <label for="first_name">First Name</label>
-                        <input type="text" id="first_name" name="first_name" placeholder="First Name" pattern="[A-Za-z]" required>
+                        <input type="text" id="fname" name="first_name" placeholder="First Name" >
+                        <span class="error"><?php echo $fname_err; ?></span>
                     </div>
                     <div class="form-group">
                         <label for="last_name">Last Name</label>
-                        <input type="text" id="last_name" name="last_name" placeholder="Last Name" pattern="[A-Za-z]" required>
+                        <input type="text" id="lname" name="last_name" placeholder="Last Name"  >
+                        <span class="error"><?php echo $lname_err; ?></span>
                     </div>
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="you@yoursite.com" pattern="[A-Za-z]" required>
+                        <input type="email" id="email" name="email" placeholder="you@yoursite.com" >
+                        <span class="error"><?php echo $email_err; ?></span>
                     </div>
                     <div class="form-group">
                         <label for="invoicenumber">Invoice Number</label>
@@ -47,27 +169,33 @@
                     <hr> </hr>
                         <div class="form-group">
                             <label for="first_name">First Name</label>
-                            <input type="text" id="first_name" name="first_name" placeholder="First Name" pattern="[A-Za-z]" required>
+                            <input type="text" id="fname" name="cfname" placeholder="First Name" >
+                            <span class="error"><?php echo $cfname_err; ?></span>
                         </div>
                         <div class="form-group">
                             <label for="last_name">Last Name</label>
-                            <input type="text" id="last_name" name="last_name" placeholder="Last Name" pattern="[A-Za-z]" required>
+                            <input type="text" id="lname" name="clname" placeholder="Last Name" >
+                            <span class="error"><?php echo $clname_err; ?></span>
                         </div>
                         <div class="form-group">
                             <label for="creditcard">Credit Card</label>
-                            <input type="number" id="ccard" name="ccard" placeholder="Credit Card Number" pattern="[1-9]{12}" required>
+                            <input type="number" id="ccard" name="ccard" placeholder="Credit Card Number" >
+                            <span class="error"><?php echo $card_err; ?></span>
                         </div>
                         <div class="form-group">
                             <label for="securitycode">Security Code</label>
-                            <input type="number" id="scode" name="scode" placeholder="Enter Security Code" pattern="[1-9]{3}" required>
+                            <input type="number" id="scode" name="scode" placeholder="Enter Security Code" >
+                            <span class="error"><?php echo $security_err; ?></span>
                         </div>
                         <div class="form-group">
                             <label for="expirationmonth">Expiration Month</label>
-                            <input type="number" id="emonth" name="emonth" placeholder="Enter Expiration Month" pattern="[1-9]{2}" required>
+                            <input type="number" id="emonth" name="emonth" placeholder="Enter Expiration Month" >
+                            <span class="error"><?php echo $month_err; ?></span>
                         </div>
                         <div class="form-group">
                             <label for="expirationyear">Expiration Year</label>
-                            <input type="number" id="eyear" name="eyear" placeholder="Enter Expiration Year" pattern="[1-9]{2}" required>
+                            <input type="number" id="eyear" name="eyear" placeholder="Enter Expiration Year" pattern="[1-9]{2}" >
+                            <span class="error"><?php echo $year_err; ?></span>
                         </div>
                 </div>
                 <div>
@@ -77,23 +205,28 @@
                     <hr> </hr>
                         <div class="form-group">
                             <label for="unitnumber">Unit/Apartment Number</label>
-                            <input type="number" id="unumber" name="unumber" placeholder="Enter Unit/Apartment Number" pattern="[1-9]{2}" required>
+                            <input type="number" id="unumber" name="unumber" placeholder="Enter Unit/Apartment Number" pattern="[1-9]{2}" >
+                            <span class="error"><?php echo $unit_err; ?></span>
                         </div>
                         <div class="form-group">
                             <label for="Street Address"> Street Address </label>
-                            <input type="text" id="saddress" name="saddress" placeholder="Enter Street Address" pattern="[A-Za-z]" required>
+                            <input type="text" id="saddress" name="saddress" placeholder="Enter Street Address" pattern="[A-Za-z]" >
+                            <span class="error"><?php echo $address_err; ?></span>
                         </div>
                         <div class="form-group">
                             <label for="city">City</label>
-                            <input type="text" id="city" name="city" placeholder="Enter City" pattern="[A-Za-z]" required>
+                            <input type="text" id="city" name="city" placeholder="Enter City" pattern="[A-Za-z]" >
+                            <span class="error"><?php echo $city_err; ?></span>
                         </div>
                         <div class="form-group">
                             <label for="state">State/Province</label>
-                            <input type="text" id="state" name="state" placeholder="Enter State/Province" pattern="[A-Za-z]" required>
+                            <input type="text" id="state" name="state" placeholder="Enter State/Province" pattern="[A-Za-z]" >
+                            <span class="error"><?php echo $state_err; ?></span>
                         </div>
                         <div class="form-group">
                             <label for="zipcode">Zip Code</label>
-                            <input type="text" id="zcode" name="zcode" placeholder="Enter Zip Code" pattern="[A-Za-z1-9]" required>
+                            <input type="text" id="zcode" name="zcode" placeholder="Enter Zip Code" pattern="[A-Za-z1-9]" >
+                            <span class="error"><?php echo $zip_err; ?></span>
                         </div>
                 </div>
                 <div>
